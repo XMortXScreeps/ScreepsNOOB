bb13137c647441358158cbc21b77c83f206346a4
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-14 16:19:19 +0000

    ewfg

--- role.harvester.js
+++ role.harvester.js
@@ -24,8 +24,7 @@ var roleHarvester = {
               creep.say('else');
                 var targets = creep.room.find(FIND_STRUCTURES, {
                   filter: (structure) => {
-                        return ((structure.structureType == STRUCTURE_CONTAINER)) &&
-                              structure.energy < structure.energyCapacity
+                        return ((structure.structureType == STRUCTURE_CONTAINER))
                             }
                           });
 
