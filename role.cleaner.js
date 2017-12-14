d9fd1de333a1bb2640aa9a03193e15e60a5ce957
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-14 10:05:31 +0000

    code correction

--- role.cleaner.js
+++ role.cleaner.js
@@ -2,7 +2,7 @@ var roleCleaner = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources[] = creep.room.find(FIND_DROPPED_RESOURCES);
+            var sources = creep.room.find(FIND_DROPPED_RESOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
