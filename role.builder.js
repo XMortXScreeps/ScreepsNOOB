0c01efcd36cc22feade0710a7c28f34ee24ef74b
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-14 11:15:54 +0000

    change var type

--- role.builder.js
+++ role.builder.js
@@ -1,7 +1,7 @@
 var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
-      var name = string.charAt(0).toUpperCase(creep.memory.role);
+      var name = String.charAt(0).toUpperCase(creep.memory.role);
       creep.say(name);
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
