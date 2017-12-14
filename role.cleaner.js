16d4d3de7c5179284f6f672f63f383ff0d464161
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-14 10:56:07 +0000

    updated cleaner to include containers

--- role.cleaner.js
+++ role.cleaner.js
@@ -3,19 +3,19 @@ var roleCleaner = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity /*&& creep.room.find(FIND_DROPPED_RESOURCES) != undefined*/) {
+	    if(creep.carry.energy < creep.carryCapacity && creep.room.find(FIND_DROPPED_RESOURCES) != undefined) {
             var dropped_energy = creep.room.find(FIND_DROPPED_RESOURCES);
             if(creep.pickup(dropped_energy[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(dropped_energy[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
-        /*else if (creep.room.find(FIND_DROPPED_RESOURCES) != undefined) {
+        else if (creep.room.find(FIND_DROPPED_RESOURCES) != undefined) {
           roleHarvester.run(creep)
-        }*/
+        }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER) &&
                             structure.energy < structure.energyCapacity;
                     }
             });
