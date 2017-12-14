39592054d1a176055977198a6b934dc36dc47374
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-14 13:01:36 +0000

    container logic altered

--- role.harvester.js
+++ role.harvester.js
@@ -9,19 +9,21 @@ var roleHarvester = {
             }
         }
         else {
+
+          if (STRUCTURE_SPAWN.energy < ((STRUCTURE_SPAWN.energyCapacity / 100) * 90)){
             var targets = creep.room.find(FIND_STRUCTURES, {
-
-
-                    filter: (structure) => {
-                      if (STRUCTURE_SPAWN.energy < ((STRUCTURE_SPAWN.energyCapacity / 100) * 90)){
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+              filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                             structure.energy < structure.energyCapacity;
-                      } else {
+                          }
+              } else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                  filter: (structure) => {
                         return (structure.structureType == STRUCTURE_CONTAINER) &&
-                            structure.energy < structure.energyCapacity;
-                      }
+                              structure.energy < structure.energyCapacity;
+                            }
+              }
 
-                    }
             });
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
