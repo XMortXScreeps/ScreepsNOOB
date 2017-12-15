fa5ebc2871c79211eae9bd8e976676b2b8691f2f
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-15 08:55:11 +0000

    uuu

--- role.harvester.js
+++ role.harvester.js
@@ -1,31 +1,31 @@
 module.exports = {
      run: function(creep) {
        creep.say(creep.memory.role);
-         if (creep.memory.working == true && creep.carry.energy == 0) {
+         if(creep.memory.working == true && creep.carry.energy == 0) {
              creep.memory.working = false;
         }
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+        else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
              creep.memory.working = true;
          }
 
 
-         if (creep.memory.working == true) {
--            if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+         if(creep.memory.working == true) {
+-            if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 -                creep.moveTo(Game.spawns.Spawn1);
 +            var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
 +                filter: (s) => s.energy < s.energyCapacity
 +            });
 +
 
-+            if (structure != undefined) {
-+                if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
++            if(structure != undefined) {
++                if(creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 +                    creep.moveTo(structure);
 +                }
              }
          }
         else {
             var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
         }
