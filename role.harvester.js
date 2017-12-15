3612d38091ef71d8faffcc6ef15fe830e812cdd6
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-15 09:09:35 +0000

    fghj

--- role.harvester.js
+++ role.harvester.js
@@ -25,12 +25,12 @@ module.exports = {
              }
          }
         else {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
         }
-      
+
     }
   }
 };
