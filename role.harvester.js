df24092ec660f96b9003a6cbffc5962b2ede9e3d
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-15 08:47:56 +0000

    et

--- role.harvester.js
+++ role.harvester.js
@@ -6,13 +6,12 @@ module.exports = {
          if (creep.memory.working == true && creep.carry.energy == 0) {
              // switch state
              creep.memory.working = false;
+        }
+        // if creep is harvesting energy but is full
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            // switch state
+             creep.memory.working = true;
          }
-         // if creep is harvesting energy but is full
-         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-           // switch state
-           creep.memory.working = true;
-         }
-
 
 -        // if creep is supposed to transfer energy to the spawn
 +        // if creep is supposed to transfer energy to the spawn or an extension
@@ -38,3 +37,15 @@ module.exports = {
 +                }
              }
          }
+         // if creep is supposed to harvest energy from source
+        else {
+            // find closest source
+            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            // try to harvest energy, if the source is not in range
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                // move towards the source
+                creep.moveTo(source);
+            }
+        }
+    }
+};
