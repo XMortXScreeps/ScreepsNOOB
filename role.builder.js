bd323b50e42c8b6a351e6cf96e393cefc3d2f694
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-15 10:00:25 +0000

    ght

--- role.builder.js
+++ role.builder.js
@@ -1,3 +1,4 @@
+var roleHarvester = require('role.harvester');
 var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -12,9 +13,11 @@ var roleBuilder = {
 	    }
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
+            if(targets.length > 0) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                } else {
+                  roleHarvester.run(creep);
                 }
             }
 	    }
