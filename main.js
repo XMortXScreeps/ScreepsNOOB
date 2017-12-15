66a1bd85ae04fcbb31a0edcce910b55802a30a2e
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-15 17:04:33 +0000

    dsf

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ module.exports.loop = function () {
     var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
     console.log('Repairers: ' + repairers.length);
 
-    if (Room.resources.find(FIND_DROPPED_RESOURCES) != undefined){
+    if (Room.find(FIND_DROPPED_RESOURCES) != undefined){
       cleanersMax = 1;
     } else {
       cleanersMax = 0;
