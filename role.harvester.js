70360f87ee66354acb74bf06fb36f4d666d0d45f
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-15 12:28:34 +0000

    dsfg

--- role.harvester.js
+++ role.harvester.js
@@ -18,11 +18,12 @@ module.exports = {
             // find closest spawn or extension which is not full
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 filter: (s) => s.energy < s.energyCapacity
-            }); +
-            creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (s) => s.store< s.storeCapacity
             });
 
+            if (structure != undefined) {
+              structure = creep.room.storage;
+            }
+            
             // if we found one
             if (structure != undefined) {
                 // try to transfer energy, if it is not in range
