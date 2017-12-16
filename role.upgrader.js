9bb036c5fb5e47e636881b2667fbdda79d94aef9
Scott Carroll <cheesecake.online@gmail.com>
2017-12-16 23:44:34 +0000

    dhvkd

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,8 +1,8 @@
 module.exports = {
     // a function to run the logic for this role
+    /** @param {Creep} creep */
     run: function(creep) {
-      creep.say(creep.memory.role);
-        // if creep is trying to repair something but has no energy left
+        // if creep is bringing energy to the controller but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
             creep.memory.working = false;
@@ -12,22 +12,21 @@ module.exports = {
             // switch state
             creep.memory.working = true;
         }
-        // if creep is supposed to repair something
-        if (creep.memory.working == true) {
 
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        // if creep is supposed to transfer energy to the controller
+        if (creep.memory.working == true) {
+            // instead of upgraderController we could also use:
+            // if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+
+            // try to upgrade the controller
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                // if not in range, move towards the controller
                 creep.moveTo(creep.room.controller);
             }
-        // if creep is supposed to harvest energy from source
+        }
+        // if creep is supposed to get energy
         else {
-            // find closest source
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                // move towards the source
-                creep.moveTo(source);
-            }
+            creep.getEnergy(true, true);
         }
     }
-}
 };
