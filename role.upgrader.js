5cb9e9b042daf8088d3cd3ea4c6e14698d598a5d
scott carroll <cheesecake.online@googlemail.com>
2017-12-16 18:42:54 +0000

    fixed upgraders to carry energy

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,18 +1,25 @@
 var roleUpgrader = {
-    /** @param {Creep} creep **/
-    run: function(creep) {
-      creep.say(creep.memory.role);
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
-            }
-        }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        }
-	}
+
+   /** @param {Creep} creep **/
+   run: function(creep) {
+       creep.say(creep.memory.role);
+   if (creep.memory.working == true && creep.carry.energy == 0) {
+           // switch state
+           creep.memory.working = false;
+       }
+       // if creep is harvesting energy but is full
+       else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+           // switch state
+           creep.memory.working = true;
+       }
+       // if creep is supposed to upgrade something
+       if (creep.memory.working == true) {
+
+           if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+               creep.moveTo(creep.room.controller);
+           }
+       }
+}
 };
+
 module.exports = roleUpgrader;
