e0be0b35e6ec7167cc8b973138820889ff54237b
scott carroll <cheesecake.online@googlemail.com>
2017-12-17 10:53:53 +0000

    Draw Path Lines

--- role.erector.js
+++ role.erector.js
@@ -17,13 +17,9 @@ module.exports = {
         }
         // if creep is supposed to repair something
         if (creep.memory.working == true) {
-            // find closest structure with less than max hits
-            // Exclude walls because they have way too many max hits and would keep
-            // our repairers busy forever. We have to find a solution for that later.
+
             var structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                // the second argument for findClosestByPath is an object which takes
-                // a property called filter which can be a function
-                // we use the arrow operator to define it
+
                 filter: (s) => s.hits < (s.hitsMax / 40) && s.structureType == STRUCTURE_WALL
             });
             // if we find one
@@ -31,7 +27,7 @@ module.exports = {
                 // try to repair it, if it is out of range
                 if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
                     // move towards it
-                    creep.moveTo(structure);
+                    creep.moveTo(structure, {visualizePathStyle: {stroke: '#ffffff'});
                 }
             }
             // if we can't fine one
--- role.repairer.js
+++ role.repairer.js
@@ -31,7 +31,7 @@ module.exports = {
                 // try to repair it, if it is out of range
                 if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
                     // move towards it
-                    creep.moveTo(structure);
+                    creep.moveTo(structure, {visualizePathStyle: {stroke: '#ffffff'});
                 }
             }
             // if we can't fine one
