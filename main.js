9924c6b940e777e6db4ead442b5b36f96801ded5
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-18 15:36:57 +0000

    wrtg

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ module.exports.loop = function () {
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep, harvesters, harvesterFloor);
+            roleHarvester.run(creep, harvesters, harvesterFloor, towerRepair);
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
