df0313a6c60fe49d85d5c040660ac99d3c1d8ed9
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-19 08:47:47 +0000

    r3g

--- function.memoryCleanup.js
+++ function.memoryCleanup.js
@@ -1,7 +1,6 @@
 module.exports = {
 
     run: function() {
-      console.log('Memory cleanup');
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
