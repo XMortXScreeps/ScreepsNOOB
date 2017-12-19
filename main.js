f37b57285ede52607dd61320abbb250a3bbebe64
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-19 13:05:32 +0000

    rhj

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ module.exports.loop = function () {
     memoryCleanup.run();
 
     for(var myRooms in Game.rooms) {
-      var room = RoomObject.room[myRooms];
+      var room = RoomObject.room.name[myRooms];
       console.log(room.creeps);
       for(var name in room.creeps) {
         var creep = room.creeps[name];
