c366078f8c3aedb3b6af78e999c185e8d4a69a66
Scott Carroll <scott.carroll@access-1LLF8JC.uk.access-accounts.com>
2017-12-22 10:54:20 +0000

    sdg

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ require('prototype.creep');
 require('prototype.tower');
 require('prototype.spawn');
 
-// Use this to change spawn memory - Game.spawns.Spawn1.memory.minCreeps = { upgrader: 4, lorry: 4, builder: 2, repairer: 2, wallRepairer: 1, harvester: 0, miner: 0, longDistanceHarvester: 0, claimer: 0 }
+// Use this to change spawn memory - Game.spawns.Spawn1.memory.minCreeps = { upgrader: 4, lorry: 4, builder: 2, repairer: 2, wallRepairer: 1, harvester: 0, miner: 0, claimer: 0 }
+// Add longDistanceHarvesters to spawn memory - Game.spawns.Spawn1.memory.minLongDistanceHarvesters = {}
 
 module.exports.loop = function() {
     // check for memory entries of died creeps by iterating over Memory.creeps
